e9cb11170ab31a9d87dcdfd68a497ee21f49b830
Swathig18 <160009692+Swathig18@users.noreply.github.com>
2025-08-15 15:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (30.38%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            int ld=digits[i];
+            if(ld<9){
+                digits[i]+=1;
+                return digits;
+            }
+               digits[i]=0;
+            }
+            int[] arr=new int[digits.length+1];
+            arr[0]=1;
+            return arr;
+
+        }
+        
+    }
