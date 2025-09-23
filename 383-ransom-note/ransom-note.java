7ce13aaffc899eccabe780fef19421386c8120dc
Swathig18 <160009692+Swathig18@users.noreply.github.com>
2025-09-23 22:32:18 +0530

    Time: 14 ms (25.63%) | Memory: 45.4 MB (15.62%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -3,13 +3,13 @@ class Solution {
        HashMap<Character,Integer> hash=new HashMap<>();
        for(char c:magazine.toCharArray()){
           hash.put(c, hash.getOrDefault(c,0)+1);
-       }
+        }
        for(char c:ransomNote.toCharArray()){
          if(!hash.containsKey(c)||hash.get(c)==0){
             return false;
-         }
-       hash.put(c,hash.get(c)-1);
         }
+       hash.put(c,hash.get(c)-1);
+    }
         return true;
    }
 }
