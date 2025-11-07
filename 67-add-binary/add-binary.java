b4ccf3bc62ee72f7eb5067d906603467e839b3f8
Swathig18 <160009692+Swathig18@users.noreply.github.com>
2025-11-07 07:21:06 +0530

    Time: 1 ms (99.75%) | Memory: 43 MB (15.08%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,17 +4,13 @@ class Solution {
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
-
         while (i >= 0 || j >= 0 || carry == 1) {
             int sum = carry;
-
             if (i >= 0) sum += a.charAt(i--) - '0';
             if (j >= 0) sum += b.charAt(j--) - '0';
-
-            sb.append(sum % 2);   // add bit
-            carry = sum / 2;      // update carry
+            sb.append(sum % 2);   
+            carry = sum / 2;     
         }
-
         return sb.reverse().toString();
     }
 }
